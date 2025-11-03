61c7ca6c33f4a851e255bc27222717ff92803f62
Kaio Augusto <114446391+KAIKOAUGUSTIN@users.noreply.github.com>
2025-11-03 15:44:36 -0300

    Refactor heartbeat.py for improved clarity
    Removed unnecessary comments and improved code formatting.

--- heartbeat.py
+++ heartbeat.py
@@ -2,17 +2,15 @@ import time
 import requests
 
 URL = "https://status.kaioaugusto.com/api/push/ViuggxT4wa?status=up&msg=OK&ping="
-INTERVAL = 20  # segundos
+INTERVAL = 20
 
 while True:
     start = time.time()
     try:
-        response = requests.get(URL, timeout=5)  # timeout evita travar indefinidamente
+        response = requests.get(URL, timeout=5)
         print(f"Heartbeat enviado. Código de status: {response.status_code}")
     except requests.RequestException as e:
         print(f"Ocorreu um erro: {e}")
-
-    # garante compasso de 20s
     elapsed = time.time() - start
     sleep_time = max(0, INTERVAL - elapsed)
     time.sleep(sleep_time)
