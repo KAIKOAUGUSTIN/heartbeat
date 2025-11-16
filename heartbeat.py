335a5dc04a49b5652f3d3125136a58ec124452ff
Kaio Augusto <114446391+KAIKOAUGUSTIN@users.noreply.github.com>
2025-11-15 23:13:09 -0300

    Update heartbeat URL to new endpoint

--- heartbeat.py
+++ heartbeat.py
@@ -1,7 +1,7 @@
 import time
 import requests
 
-URL = "https://status.kaioaugusto.com/api/push/KEoXTMAwvImLkeCp6LVWFgrp1HmquhkO?status=up&msg=OK&ping="
+URL = "http://193.149.164.213:1372/api/push/KEoXTMAwvImLkeCp6LVWFgrp1HmquhkO?status=up&msg=OK&ping="
 INTERVAL = 20
 
 while True:
