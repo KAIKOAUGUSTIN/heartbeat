c86d9dbf603a89d28cb706f309db88cd54141225
Kaio Augusto <114446391+KAIKOAUGUSTIN@users.noreply.github.com>
2026-02-01 00:46:30 -0300

    Update heartbeat URL to new endpoint

--- heartbeat.py
+++ heartbeat.py
@@ -1,7 +1,7 @@
 import time
 import requests
 
-URL = "http://193.149.164.213:1372/api/push/5UE0PwTK8J5VKEcs16aBmHBBGsZsTH3s?status=up&msg=OK&ping="
+URL = "https://uptime.betterstack.com/api/v1/heartbeat/j65Zh9SrEgMichFbPyuqjqnE"
 INTERVAL = 20
 
 while True:
